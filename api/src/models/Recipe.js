18dda74b3eb5c5c9e49f0369898fd6946e3b1ec3
sabry <sabrysubelza@gmail.com>
2022-11-20 12:36:12 -0300

    -m "guardar pi"

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('recipe', {
+    // ID: *
+		recipe_id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+			allowNull: false,
+		},
+		// Nombre *
+		recipe_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+            unique: true,
+		},
+		// Resumen del plato *
+		dish_description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// Puntuación
+		score: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		// Nivel de "comida saludable"
+		healthy_food_level: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		// Paso a paso
+		step_by_step: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+		},
+		image: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+		},
+		// Recipientes creados en la base de datos
+		created_in_db: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: true,
+			allowNull: false,
+		},
+	});
+};
---
+++ api/src/models/TypeOfDiet.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+	sequelize.define('type_of_diet', {
+		// type_of_diet_id: {
+		// 	type: DataTypes.UUID,
+		// 	defaultValue: DataTypes.UUIDV4,
+		// 	primaryKey: true,
+		// 	allowNull: false,
+		// },
+		// Nombre *
+		type_of_diet_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+            unique: true,
+		},
+	});
+};
